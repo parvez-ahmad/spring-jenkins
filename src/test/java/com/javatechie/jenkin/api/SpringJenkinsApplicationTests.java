8c78cc63d0dd225c18563d2371d098a3333a3677
parvez09ahmad <parveziahmad09@gmail.com>
2021-06-08 11:03:28 +0530

    success

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -17,7 +17,7 @@ public class SpringJenkinsApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	public contextLoads() {
+	public void contextLoads() {
 		logger.info("Test case executing...");
 		//logger.info("test cases executed second log statement");
 		assertEquals(true, true);
